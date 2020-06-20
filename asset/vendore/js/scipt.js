5af7c593ce4f9f5b61a909b685cd0d0faf32814c
aninarafath6 <arafathanin6@gmail.com>
2020-06-20 16:24:00 -0400

    responsive

--- asset/vendore/js/scipt.js
+++ asset/vendore/js/scipt.js
@@ -11,7 +11,36 @@ let h1=document.getElementById("h1");
 let section=0;
 let mark=0;
 let chekker=0;   
-
+function mark_print(){
+    h1.innerHTML=mark+"/"+4;
+    questions_hedder.innerHTML="complited";
+    
+    mark=0;
+        section=0
+    setTimeout(() => {
+        btn_1.style.border='2px solid rgba(251,32,86,1)';
+    
+        btn_1.style.backgroundColor="#fff"
+        btn_1.style.Color="black";
+        button();
+        question.innerHTML="1)Which heading tag will produce the largest text?";
+        btn_1.innerHTML="h1 tag";
+        btn_2.innerHTML="h3 tag";
+        btn_3.innerHTML="h6 tag";
+        btn_4.innerHTML="h2 tag";
+        questions_hedder.innerHTML="Questions No:1";
+        hidden.style.visibility="hidden"
+        next.style.visibility="visible";
+        mark=0;
+        section=0
+        
+       
+        
+    }, 2000);
+    
+    
+    }
+    
 
 function next_button(){
     if (section==0&&chekker==1) {
@@ -19,7 +48,7 @@ function next_button(){
         btn_1.innerHTML="img tag";
         btn_2.innerHTML="ol tag";
         btn_3.innerHTML="a tag";
-        btn_4.innerHTML="table tag";
+        btn_4.innerHTML="ul tag";
         btn_1.style.background='#ffffff';
         btn_1.style.border='2px solid rgba(251,32,86,1)';
         btn_1.style.color="#000000";
@@ -51,7 +80,7 @@ function next_button(){
         question.innerHTML="3)To make your website mobile friendly, you can make your website______";
         btn_1.innerHTML="Light";
         btn_2.innerHTML="Reactive";
-        btn_3.innerHTML="Fast Loading";
+        btn_3.innerHTML="Loading";
         btn_4.innerHTML="Responsive";
         btn_3.style.background='#ffffff';
         btn_3.style.border='2px solid rgba(251,32,86,1)';
@@ -120,13 +149,10 @@ function button(trf,btn_id){
     chekker=1;
    
     if(trf&&btn_id===0&&section===0){
+        
         mark=0;
         mark++;
-        questions_hedder.innerHTML="Correct Answer";
-        
-        setTimeout(() => {
-            questions_hedder.innerHTML="Questions No:1";
-        }, 2000);
+       
         console.log(mark);
         btn_1.style.background='rgb(3, 100, 0)';
         btn_1.style.color='#ffffff';
@@ -148,10 +174,7 @@ function button(trf,btn_id){
          btn_1.style.background='rgb(3, 100, 0)';
         btn_1.style.color='#ffffff';
         btn_1.style.border='1px solid #ffff';
-        questions_hedder.innerHTML="Wrong Answer";
-        setTimeout(() => {
-            questions_hedder.innerHTML="Questions No:1";
-        }, 2000);
+   
        
         next.style.color='#ffffff';
         next.style.backgroundColor="  rgba(251,32,86,1)";
@@ -167,10 +190,7 @@ function button(trf,btn_id){
         mark++;
           
         
-            questions_hedder.innerHTML="Correct Answer";
-            setTimeout(() => {
-                questions_hedder.innerHTML="Questions No:2";
-            }, 2000);
+           
             console.log(mark);
             btn_3.style.background='rgb(3, 100, 0)';
             btn_3.style.color='#ffffff';
@@ -187,12 +207,7 @@ function button(trf,btn_id){
     else if(btn_id>2&&section==1||btn_id<2&&section==1){
         chekker=2;
         btn_3.style.pointerEvents="none";
-        questions_hedder.innerHTML="Wrong Answer";
-        setTimeout(() => {
-            questions_hedder.innerHTML="Questions No:2";
-        }, 2000);
-        questions_hedder.innerHTML="Questions No:2";
-
+      
         btn_3.style.background='rgb(3, 100, 0)';
         btn_3.style.color='#ffffff';
         btn_3.style.border='1px solid #ffff';
@@ -207,10 +222,7 @@ function button(trf,btn_id){
    else if (btn_id==3&&section==2) {
     chekker=3;
     mark++;
-    questions_hedder.innerHTML="Correct Answer";
-    setTimeout(() => {
-        questions_hedder.innerHTML="Questions No:3";
-    }, 2000);
+ 
     console.log(mark);
     btn_4.style.background='rgb(3, 100, 0)';
     btn_4.style.color='#ffffff';
@@ -223,10 +235,7 @@ function button(trf,btn_id){
    } else if (btn_id<3&&section==2) {
     chekker=3;
     btn_4.style.pointerEvents="none";
-    questions_hedder.innerHTML="Wrong Answer";
-    setTimeout(() => {
-        questions_hedder.innerHTML="Questions No:3";
-    }, 2000);
+   
     console.log(mark);
     btn_4.style.background='rgb(3, 100, 0)';
     btn_4.style.color='#ffffff';
@@ -238,11 +247,7 @@ function button(trf,btn_id){
    }else if (btn_id==0&&section==3) {
     chekker=4;
     mark++;
-    questions_hedder.innerHTML="Correct Answer";
-    setTimeout(() => {
-        questions_hedder.innerHTML="Questions No:4";
-    }, 2000);
-    console.log(mark);
+    (mark);
     btn_1.style.background='rgb(3, 100, 0)';
     btn_1.style.color='#ffffff';
     btn_1.style.border='1px solid #ffff';
@@ -250,6 +255,9 @@ function button(trf,btn_id){
         hidden.style.visibility="visible"
         
         mark_print();
+        
+        btn_1.style.backgroundColor="#fff"
+        btn_1.style.Color="black";
     }, 2000);
     next.style.color='#ffffff';
     next.style.backgroundColor="  rgba(251,32,86,1)";
@@ -262,14 +270,14 @@ function button(trf,btn_id){
      btn_1.style.background='rgb(3, 100, 0)';
     btn_1.style.color='#ffffff';
     btn_1.style.border='1px solid #ffff';
-    questions_hedder.innerHTML="Wrong Answer";
-    setTimeout(() => {
-        questions_hedder.innerHTML="Questions No:4";
-    }, 2000); 
+     
     setTimeout(() => {
         hidden.style.visibility="visible"
     
         mark_print();
+        
+        btn_1.style.backgroundColor="#fff"
+        btn_1.style.Color="black";
     }, 2000);
 
     next.style.color='#ffffff';
@@ -283,32 +291,6 @@ function button(trf,btn_id){
 }
 
    
-}
-function mark_print(){
-h1.innerHTML=mark+"/"+4;
-questions_hedder.innerHTML="complited";
-mark=0;
-    section=0
-setTimeout(() => {
-    hidden.style.visibility="hidden"
-    next.style.visibility="visible";
-    mark=0;
-    section=0
-    
-    btn_1.style.border='2px solid rgba(251,32,86,1)';
-
-    btn_1.style.backgroundColor="#fff"
-    btn_1.style.Color="black";
-    button();
-    question.innerHTML="1)Which heading tag will produce the largest text?";
-    btn_1.innerHTML="h1 tag";
-    btn_2.innerHTML="h3 tag";
-    btn_3.innerHTML="h6 tag";
-    btn_4.innerHTML="h2 tag";
-    
-}, 2000);
-
-
 }
 
 
